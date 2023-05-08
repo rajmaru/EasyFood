55848240cd9f3c2678fc08e0f24b7b8d2786400c
Raj Maru <rajmaru0304@gmail.com>
2023-05-08 19:49:23 +0530

    Initial commit

---
+++ app/src/main/java/com/one/easyfood/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.one.easyfood
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.one.easyfood.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+        NavigationUI.setupWithNavController(binding.btmNav, navController)
+    }
+}
