409f0e66c685151dce81bdc3927fd0bdda1eeb42
Raj Maru <rajmaru0304@gmail.com>
2023-06-03 19:16:06 +0530

    video view

--- app/src/main/java/com/one/easyfood/VideoView.kt
+++ app/src/main/java/com/one/easyfood/VideoView.kt
@@ -2,71 +2,39 @@ package com.one.easyfood
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.MediaController
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.source.dash.DashMediaSource
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.one.easyfood.databinding.ActivityVideoViewBinding
 
-class VideoView : AppCompatActivity(){
-
+class VideoView : AppCompatActivity() {
     private lateinit var binding: ActivityVideoViewBinding
-    private var exoPlayer: ExoPlayer? = null
-    private var playbackPosition = 0L
-    private var playWhenReady = true
     private var url: String? = null
+    private lateinit var mediaController: MediaController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVideoViewBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val view = binding.root
+        setContentView(view)
 
         getUrl()
         preparePlayer()
     }
 
     private fun getUrl() {
-        url = intent.getStringExtra("MEAL_YOUTUBE_LINK").toString()
+        //   url = intent.getStringExtra("MEAL_YOUTUBE_LINK").toString()
+        url = "https://www.youtube.com/ewdSmjMbsP0"
+        Log.d("LINK", url!!)
     }
 
     private fun preparePlayer() {
-        exoPlayer = ExoPlayer.Builder(this).build()
-        exoPlayer?.playWhenReady = true
-        binding.playerView.player = exoPlayer
-        val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
         val uri = Uri.parse(url)
-        val mediaItem = MediaItem.fromUri(uri)
-        val mediaSource = DashMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(mediaItem)
-        exoPlayer?.setMediaSource(mediaSource)
-        exoPlayer?.playWhenReady = playWhenReady
-        exoPlayer?.prepare()
+        mediaController = MediaController(this)
+        binding.videoView.setVideoURI(uri)
+        binding.videoView.start()
+
+        binding.videoView.setMediaController(mediaController)
+        mediaController.setAnchorView(binding.videoView)
     }
-
-
-    private fun  relasePlayer(){
-        exoPlayer?.let { player ->
-            playbackPosition = player.currentPosition
-            playWhenReady = player.playWhenReady
-            player.release()
-            exoPlayer = null
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        relasePlayer()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        relasePlayer()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        relasePlayer()
-    }
-
-
 }
